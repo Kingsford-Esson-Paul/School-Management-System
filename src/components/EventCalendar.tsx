fb44d3ad6110b25852763e1624ce3cc0169cda4d
Kingsford Esson Paul <123348644+Kingsford-Esson-Paul@users.noreply.github.com>
2024-11-21 07:15:16 +0000

    Dashboard page content updated

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -13,19 +13,19 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 const events = [
   {
     id: 1,
-    title: "Lorem ipsum dolor",
+    title: "Title",
     time: "12:00 PM - 2:00 PM",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     id: 2,
-    title: "Lorem ipsum dolor",
+    title: "Title",
     time: "12:00 PM - 2:00 PM",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     id: 3,
-    title: "Lorem ipsum dolor",
+    title: "Title",
     time: "12:00 PM - 2:00 PM",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
